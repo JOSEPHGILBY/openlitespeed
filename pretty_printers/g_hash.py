3b2bb68bd87f3c5c7e3d0e195cdebd9c4104184a
Joseph Gilby <josephgilbyiv@gmail.com>
2023-10-30 20:33:37 +0000

    another weird rebuild

--- pretty_printers/g_hash.py
+++ pretty_printers/g_hash.py
@@ -4,19 +4,29 @@ class GHashPrinter:
     def __init__(self, val):
         self.val = val
 
+    def display_hint(self):
+        return 'map'
+
     def to_string(self):
-        eval_string = "(*("+str(self.val.type)+"*)("+str(self.val.address)+")).begin()"
-        first = gdb.parse_and_eval(eval_string)
-        return "str(first)"
-        # result = 'GHash:\n'
-        # begin = self.val'begin'
-        # end = self.val'end'
-        # while begin != end:
-        #     key = begin.dereference()'getKey'
-        #     data = begin.dereference()'getData'
-        #     result += 'Key: {}, Value: {}\n'.format(key, data)
-        #     begin = self.val'next'
-        # return result
+        return "GHash with {} elements".format(self.val['sizenow'])
+    
+    def children(self):
+        result = []
+        current = gdb.parse_and_eval(to_cast_string(self.val) + ".begin()")
+        end = gdb.parse_and_eval(to_cast_string(self.val) + ".end()")
+        while current != end:
+            key = current['pkey']
+            data = current['pdata']
+            result.append((str(key), data))
+            current = gdb.parse_and_eval(to_cast_string(self.val) + ".next(" + to_cast_string(current) + ")" )
+        return iter(result)
 
 def build_pretty_printer():
     return GHashPrinter
+
+def to_cast_string(gdb_val):
+    if gdb_val.type.code != gdb.TYPE_CODE_PTR:
+        addr = gdb_val.address
+        return "(*({}*)({}))".format(gdb_val.type, addr)
+    else:
+        return "(({})({}))".format(gdb_val.type, gdb_val)
--- pretty_printers/hash_string_map.py
+++ pretty_printers/hash_string_map.py
@@ -1,31 +1,25 @@
 import gdb.printing
+from g_hash import GHashPrinter
 class HashStringMapPrinter:
     def __init__(self, val):
+        ghash_val = gdb.parse_and_eval("(*({}*)({}))".format("GHash", val.address))
+        self.g_hash_printer = GHashPrinter(ghash_val)
         self.val = val
+        self.template_type = str(val.type.template_argument(0))
 
     def to_string(self):
-        return str(self.val.type)
-        # result = 'HashStringMap:\n'
-        # begin = self.val'begin'
-        # end = self.val'end'
-        # while begin != end:
-        #     key = begin.dereference()'first'
-        #     data = begin.dereference()'second'
-        #     result += 'Key: {}, Value: {}\n'.format(key, data)
-        #     begin = self.val'next'
-        # return result
+        return "it's a hashstringmap"
 
-    # def display_hint(self):
-    #     return 'map'
+    def display_hint(self):
+        return 'map'
 
-    # def children(self):
-    #     count = 0
-    #     result = []
-    #     for it in self.val:
-    #         count += 1
-    #         result.append(('Key %d' % count, it['first']))
-    #         result.append(('Value %d' % count, it['second']))
-    #     return iter(result)
+    def children(self):
+        result = []
+        for key, value in self.g_hash_printer.children():
+            casted_value = gdb.parse_and_eval("(*({})({}))".format(self.template_type, value.address))
+            result.append((key, casted_value))
+        return iter(result)
+        
 
 def build_pretty_printer():
     return HashStringMapPrinter
